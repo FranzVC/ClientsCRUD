446aaf83ad47b63af2079c3dafa10a50b0fd16dd
Franz Vilela <franz.vilela@bolivia.assuresoft.com>
2019-08-12 12:03:31 -0400

    design

--- app/src/main/java/com/e/crud_sqlite/MainActivity.java
+++ app/src/main/java/com/e/crud_sqlite/MainActivity.java
@@ -4,13 +4,22 @@ import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
+import com.e.crud_sqlite.fragment.DeleteFragment;
+import com.e.crud_sqlite.fragment.RegisterFragment;
+import com.e.crud_sqlite.fragment.SearchFragment;
+import com.e.crud_sqlite.fragment.UpdateFragment;
+import com.e.crud_sqlite.fragment.ViewFragment;
 import com.e.crud_sqlite.helper.ConnectionSQLiteHelper;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.sql.SQLData;
@@ -19,7 +28,25 @@ import static com.e.crud_sqlite.utility.ClientUtility.TABLE_CLIENTS;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
+    private RegisterFragment registerFragment;
+    private UpdateFragment updateFragment;
+    private DeleteFragment deleteFragment;
+    private SearchFragment searchFragment;
+    private ViewFragment viewFragment;
 
+    private EditText tbx_nameRegister, tbx_emailRegister, tbx_telephoneRegister;
+    private Button btn_submitRegister;
+
+    private EditText tbx_nameUpdate, tbx_emailUpdate, tbx_telephoneUpdate;
+    private Button btn_submitUpdate;
+
+    private EditText tbx_idDelete;
+    private Button btn_delete;
+
+    private EditText tbx_name, tbx_email, tbx_telephone;
+    private Button btn_search;
+
+    private FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -27,18 +54,28 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_register:
+                    fragmentTransaction.replace(R.id.fragmentContainer, registerFragment);
+                    fragmentTransaction.commit();
                     mTextMessage.setText(R.string.register_btn);
                     return true;
                 case R.id.navigation_delete:
+                    fragmentTransaction.replace(R.id.fragmentContainer, deleteFragment);
+                    fragmentTransaction.commit();
                     mTextMessage.setText(R.string.delete_btn);
                     return true;
                 case R.id.navigation_show:
+                    fragmentTransaction.replace(R.id.fragmentContainer, viewFragment);
+                    fragmentTransaction.commit();
                     mTextMessage.setText(R.string.view_btn);
                     return true;
                 case R.id.navigation_search:
+                    fragmentTransaction.replace(R.id.fragmentContainer, searchFragment);
+                    fragmentTransaction.commit();
                     mTextMessage.setText(R.string.search_btn);
                     return true;
                 case R.id.navigation_update:
+                    fragmentTransaction.replace(R.id.fragmentContainer, updateFragment);
+                    fragmentTransaction.commit();
                     mTextMessage.setText(R.string.update_btn);
                     return true;
             }
@@ -49,15 +86,26 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        registerFragment = new RegisterFragment();
+        updateFragment = new UpdateFragment();
+        deleteFragment = new DeleteFragment();
+        searchFragment = new SearchFragment();
+        viewFragment = new ViewFragment();
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        ConnectionSQLiteHelper connectionSQLiteHelper = new ConnectionSQLiteHelper(this,TABLE_CLIENTS,null,1);
+        ConnectionSQLiteHelper connectionSQLiteHelper = new ConnectionSQLiteHelper(this, TABLE_CLIENTS, null, 1);
         SQLiteDatabase sqLiteDatabase = connectionSQLiteHelper.getWritableDatabase();
 
         ContentValues values = new ContentValues();
+
+
     }
 
+    public void onClickSubmitBtn(View v) {
+
+    }
 }
